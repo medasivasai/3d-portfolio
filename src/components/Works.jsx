f084a5e34122c7153e924a3dc8d9689269452400
Siva Sai <medasivasai29@gmail.com>
2023-04-10 20:11:14 +0530

    started work section and some changes to previous sections

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../const';
+import { fadeIn,textVariant } from '../utils/motion';
+// import { fade } from 'maath/dist/declarations/src/misc';
 
+const ProjectCard = ({index,name,description,tags,image,source_code_link}) => {
+  return(
+    <motion.div variants={fadeIn('up', "spring",index*0.5,0.75)}>
+      <Tilt
+        options={{
+          max:45,
+          scale:1,
+          speed:450
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+
+      </Tilt>
+    </motion.div>
+  )
+}
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+       <motion.div variants={textVariant()}>
+            <p className={styles.sectionSubText}>My Work</p>
+            <h2 className={styles.sectionHeadText}>Prjects.</h2>
+        </motion.div>
+
+        <div className='w-full flex'>
+          <motion.p
+            variants={fadeIn("","",0.1,1)}
+            className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          >
+              Follwing Projects showcases my skills and experience through real-world examples of my work. Each project is breifly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies and manage projects effectively.
+          </motion.p>
+        </div>
+
+        <div className='mt-20 flex flex-wrap gap-7'>
+          {projects.map((project , index) => (
+            <ProjectCard key = {`project-${index}`} index={index} {...project} />
+          ))}
+        </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works , 'work');
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,7 +13,7 @@ function HOC() {
             viewport={{once:true,amount:0.25}}
             className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
         >
-            <span className='hash-span' id={idName}> &nbsp </span>
+            <span className='hash-span' id={idName}> &nbsp; </span>
             <Component/>
         </motion.section>
     )
