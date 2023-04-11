eee700ed32d7ffa0d63d36973d46336614efd795
Siva Sai <medasivasai29@gmail.com>
2023-04-11 11:46:48 +0530

    completed the portolio

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,9 @@ import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
-import Experience from './Experience';
+// import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
+// import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
@@ -13,9 +13,7 @@ export {
   Navbar,
   About,
   Tech,
-  Experience,
   Works,
-  Feedbacks,
   Contact,
   EarthCanvas, 
   BallCanvas, 
